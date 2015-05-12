090635b733716c07cf7b1b07ce9ecafc2d4c2d85
Michael Swartz <mjswartz@gmail.com>
2015-05-12 13:27:04 -0400

    messages connected to chats, but requires refresh"

--- messaging.js
+++ messaging.js
@@ -2,7 +2,10 @@ Messages = new Mongo.Collection("messages");
 Chats = new Mongo.Collection("chats");
 
 if (Meteor.isClient) {
-  Meteor.subscribe("messages");
+  Tracker.autorun(function () {
+    Meteor.subscribe("messages"); // {chat: Session.get("selectedChat")}
+  });
+
   Meteor.subscribe("chats");
 
   Template.message.helpers({
@@ -11,12 +14,12 @@ if (Meteor.isClient) {
     }
   });
 
-  // Template.chats.helpers({
-  //   selectedChat: function () {
-  //     var chat = Chats.findOne(Session.get("selectedChat"));
-  //     return chat && chat.name;
-  //   }
-  // });
+  Template.chatList.helpers({
+    selectedChat: function () {
+      selectedChat = Chats.findOne(Session.get("selectedChat"));
+      return selectedChat && selectedChat.name;
+    }
+  });
 
   Template.body.helpers({
     messages: function () {
@@ -28,10 +31,28 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.chat.helpers({
+    selected: function () {
+      return Session.equals("selectedChat", this._id) ? "selected" : '';
+    }
+  });
+
+  Template.chat.events({
+    'click': function () {
+      var chat = this._id;
+      Session.set("selectedChat", chat);
+      var user = Meteor.userId();
+      Meteor.users.update({_id: user}, {$set: {'profile.selectedChat': chat}});
+      // SelectedChat = this._id;
+    }
+  });
+
   Template.body.events({
     "submit .new-message": function (event) {
       var text = event.target.text.value;
-      Meteor.call("addMessage", text);
+      var chat = Session.get("selectedChat");
+
+      Meteor.call("addMessage", text, chat);
 
       event.target.text.value = "";
       return false;
@@ -59,13 +80,31 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    Meteor.publish("messages", function () {
-      // check(chatId, String);
-      return Messages.find();
-    });
     Meteor.publish("chats", function() {
       return Chats.find();
     });
+
+    Meteor.publish("messages", function () {
+      var user = Meteor.users.findOne(this.userId);
+      var selectedChat = user.profile.selectedChat;
+      // var selectedChat = Chats.findOne(Session.get("selectedChat"));
+      return Messages.find({ chat: selectedChat });
+    });
+
+      // check(chatId, String);
+      // if ( typeof SelectedChat !== 'undefined') {
+      //   console.log(SelectedChat);
+      //   return Messages.find({ chat: SelectedChat });
+      // } else {
+      //   return Messages.find();
+      // }
+      // if ( typeof Meteor.user().profile.iguana !== 'undefined' ) {
+      //   var selectedChat = Meteor.user().profile.selectedChat;
+      //   return Messages.find({ chat: selectedChat });
+      // } else {
+        // return Messages.find();
+      // }
+    // });
   });
 }
 
@@ -82,7 +121,7 @@ Meteor.methods({
     });
   },
 
-  addMessage: function (text) {
+  addMessage: function (text, chat) {
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
@@ -91,15 +130,16 @@ Meteor.methods({
       text: text,
       createdAt: new Date(),
       author: Meteor.userId(),
-      username: Meteor.user().username || Meteor.user().profile.name
+      username: Meteor.user().username || Meteor.user().profile.name,
+      chat: chat
     });
   },
 
   deleteMessage: function (messageId) {
-    var message = Messages.findOne(messageId);
-    if (message.author !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
+    // var message = Messages.findOne(messageId);
+    // if (message.author !== Meteor.userId()) {
+    //   throw new Meteor.Error("not-authorized");
+    // }
 
     Messages.remove(messageId);
   }
